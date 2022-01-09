a81898486cbd98512d7d88b3007e2760d06caedf
MajorArkwolf <petercrabbe92@gmail.com>
2022-01-09 17:37:27 +0800

    fix member variable updates

--- OCRScripts/camera.py
+++ OCRScripts/camera.py
@@ -1,14 +1,27 @@
 import time
-import picamera
+camera_lib_loaded = None
+try:
+    import picamera
+    camera_lib_loaded = True
+except ModuleNotFoundError as err:
+    camera_lib_loaded = False
+
 import numpy as np
 import cv2
-
+p
 class Camera:
     def __init__(self):
-        self.camera = picamera.PiCamera()
-        self.resolution.x = 2592
-        self.resolution.y = 1944
-        self.camera.framerate = 24
+        if camera_lib_loaded == True:
+            self.camera = picamera.PiCamera()
+            self.camera.framerate = 24
+            self.camera.resolution.x = 2592
+            self.camera.resolution.y = 1944
+            self.camera.rotation = 180
+        self.loaded = camera_lib_loaded
+
+    def __del__(self):
+        if camera_lib_loaded == True:
+            self.camera.close()
 
     def capture_opencv(self):
         image = np.empty((self.resolution.y * self.resolution.x * 3,), dtype=np.uint8)
