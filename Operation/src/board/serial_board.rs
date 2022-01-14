3fcec218f35e1bedc174bcf74d6f20ee5d2ef81c
MajorArkwolf <petercrabbe92@gmail.com>
2022-01-14 15:40:17 +0800

    change up serial port logic

--- Operation/src/board/serial_board.rs
+++ Operation/src/board/serial_board.rs
@@ -2,15 +2,50 @@ use crate::board::network::SerialTemplate;
 
 use super::{arduino_board::ArduinoBoard, BoardTypes};
 use color_eyre::eyre::{eyre, Result, WrapErr};
-use core::time;
 use firmata::Firmata;
 use std::sync::Arc;
 use tokio::sync::Mutex;
-use tokio_serial::{DataBits, Parity, SerialPort, StopBits};
-use tracing::error;
+use tokio_serial::{DataBits, Parity, SerialPort, SerialPortInfo, StopBits};
+use tracing::{error, warn};
 
 pub fn generate(port: Box<dyn SerialPort>) -> ArduinoBoard<Box<dyn SerialPort>> {
     ArduinoBoard::new(Arc::new(Mutex::new(firmata::Board::new(port))))
+}
+
+fn convert_dir_to_path(path: &str) -> Result<String> {
+    let tokens = path.split('/').collect::<Vec<&str>>();
+    let mut linux_path: String = "/dev/".to_owned();
+    let name = tokens
+        .last()
+        .ok_or_else(|| eyre!("no last element found"))?;
+    linux_path.push_str(name);
+    Ok(linux_path)
+}
+
+fn generate_port(port_info: &SerialPortInfo, baud_rate: u32) -> Result<Box<dyn SerialPort>> {
+    match tokio_serial::new(&port_info.port_name, baud_rate)
+        .data_bits(DataBits::Eight)
+        .parity(Parity::None)
+        .stop_bits(StopBits::One)
+        .open()
+    {
+        Ok(x) => Ok(x),
+        Err(_) => match convert_dir_to_path(&port_info.port_name) {
+            Ok(_) => match tokio_serial::new(&port_info.port_name, baud_rate)
+                .data_bits(DataBits::Eight)
+                .parity(Parity::None)
+                .stop_bits(StopBits::One)
+                .open()
+            {
+                Ok(v) => Ok(v),
+                Err(e) => Err(eyre!(
+                    "failed to connect to port after changing path {:?}",
+                    e
+                )),
+            },
+            Err(_) => Err(eyre!("failed to generate a new path name")),
+        },
+    }
 }
 
 pub fn generate_serial_board(template: SerialTemplate, identifier: String) -> Result<BoardTypes> {
@@ -20,18 +55,11 @@ pub fn generate_serial_board(template: SerialTemplate, identifier: String) -> Re
     let mut tried = 0;
     let mut skipped = 0;
     for p in ports {
-        let comm = match tokio_serial::new(&p.port_name, template.baud_rate)
-            .data_bits(DataBits::Eight)
-            .parity(Parity::None)
-            .stop_bits(StopBits::One)
-            .open()
-        {
-            Ok(x) => x,
+        let comm = match generate_port(&p, template.baud_rate) {
+            Ok(v) => v,
             Err(e) => {
+                warn!("Comm port failed: {:?}", e);
                 skipped += 1;
-                devices_found.push(' ');
-                devices_found.push_str(&p.port_name);
-                devices_found.push_str(&e.description);
                 continue;
             }
         };
@@ -51,8 +79,8 @@ pub fn generate_serial_board(template: SerialTemplate, identifier: String) -> Re
     }
 
     error!(
-        "Tried: {}, Skipped {}. Failed to find `{}`: found `{}`",
-        tried, skipped, identifier, devices_found
+        "Tried: {}, Skipped {}. Failed to find `{}`",
+        tried, skipped, identifier
     );
     Err(eyre!("failed to find board"))
 }
