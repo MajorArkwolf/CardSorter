000114f98066316dcc519cf014291b0378c50a3c
MajorArkwolf <petercrabbe92@gmail.com>
2022-01-15 21:24:03 +0800

    impl more calibration

--- Operation/src/circuit/circuit_constructor.rs
+++ Operation/src/circuit/circuit_constructor.rs
@@ -1,18 +1,73 @@
 use std::sync::Arc;
 use std::vec;
 
+use super::circuit_controller::CircuitController;
+use super::Circuit;
+use super::CircuitState;
 use crate::board;
 use crate::circuit;
 use board::BoardContainer;
 use circuit::{capture::Capture, feeder::Feeder};
+use color_eyre::eyre::eyre;
 use color_eyre::eyre::Result;
 use tokio::sync::Mutex;
 
-use super::circuit_controller::CircuitController;
-use super::Circuit;
-use super::CircuitState;
+struct CalibrationResult {
+    sensor_id: u32,
+    sensor_calibration_value: i32,
+}
 
-pub fn construct_feeder(boards: &mut BoardContainer) -> Result<Feeder> {
+fn calibrate_photo_resistor(
+    boards: &mut BoardContainer,
+    sensor_id: u32,
+) -> Result<CalibrationResult> {
+    let photo = boards
+        .get_sensor(sensor_id)?
+        .as_photo_resistor_mut()
+        .unwrap();
+
+    //turn light off
+    let mut off_average = 0;
+    for _i in 1..20 {}
+    off_average /= 20;
+    //turn light on
+    let mut on_average = 0;
+    for _i in 1..20 {}
+    on_average /= 20;
+
+    if off_average > on_average {
+        return Err(eyre!(
+            "off average `{}` was larger then on average `{}`",
+            off_average,
+            on_average
+        ));
+    }
+
+    let half_way_point: i32 = ((on_average - off_average) / 2) + off_average;
+
+    if half_way_point > on_average {
+        return Err(eyre!(
+            "mid point `{}` was larger then on max average `{}`",
+            half_way_point,
+            on_average
+        ));
+    }
+
+    Ok(CalibrationResult {
+        sensor_id,
+        sensor_calibration_value: half_way_point,
+    })
+}
+
+fn calibrate_sensors(boards: &mut BoardContainer) -> Result<Vec<CalibrationResult>> {
+    let calibration_results: Vec<CalibrationResult> = vec![calibrate_photo_resistor(boards, 2)?];
+    Ok(calibration_results)
+}
+
+fn construct_feeder(
+    boards: &mut BoardContainer,
+    calibration_results: &[CalibrationResult],
+) -> Result<Feeder> {
     let motor_cont = boards
         .get_sensor(3)?
         .as_motor_controller_mut()
@@ -24,30 +79,60 @@ pub fn construct_feeder(boards: &mut BoardContainer) -> Result<Feeder> {
         .unwrap()
         .subscribe();
     let servo = boards.get_sensor(1)?.as_servo_mut().unwrap().publisher();
+
+    let value = calibration_results
+        .iter()
+        .find(|x| x.sensor_id == 2)
+        .ok_or_else(|| eyre!("failed to find a calibration result that matches the id"))?
+        .sensor_calibration_value;
+
     Ok(Feeder::create(
         0,
         CircuitState::Waiting,
         motor_cont,
         photo,
         servo,
-        500,
+        value,
     ))
 }
 
-pub fn construct_capture(boards: &mut BoardContainer) -> Result<Capture> {
+fn construct_capture(
+    boards: &mut BoardContainer,
+    calibration_results: &[CalibrationResult],
+) -> Result<Capture> {
     let servo = boards.get_sensor(1)?.as_servo_mut().unwrap().publisher();
     let photo = boards
         .get_sensor(2)?
         .as_photo_resistor_mut()
         .unwrap()
         .subscribe();
-    Ok(Capture::create(1, CircuitState::Waiting, servo, photo, 500))
+
+    let value = calibration_results
+        .iter()
+        .find(|x| x.sensor_id == 2)
+        .ok_or_else(|| eyre!("failed to find a calibration result that matches the id"))?
+        .sensor_calibration_value;
+
+    Ok(Capture::create(
+        1,
+        CircuitState::Waiting,
+        servo,
+        photo,
+        value,
+    ))
 }
 
 pub fn construct_circuit(boards: &mut BoardContainer) -> Result<CircuitController> {
+    let calibration_results = calibrate_sensors(boards)?;
     let circuits: Vec<Arc<Mutex<Box<dyn Circuit + Send>>>> = vec![
-        Arc::new(Mutex::new(Box::new(construct_feeder(boards)?))),
-        Arc::new(Mutex::new(Box::new(construct_capture(boards)?))),
+        Arc::new(Mutex::new(Box::new(construct_feeder(
+            boards,
+            &calibration_results,
+        )?))),
+        Arc::new(Mutex::new(Box::new(construct_capture(
+            boards,
+            &calibration_results,
+        )?))),
     ];
     Ok(CircuitController::create(circuits))
 }
