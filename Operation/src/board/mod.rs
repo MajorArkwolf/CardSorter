bc35e0429b784f377f985746d96d3550a576be52
MajorArkwolf <petercrabbe92@gmail.com>
2022-01-26 14:42:59 +0800

    use network to connect to board

--- Operation/src/board/mod.rs
+++ Operation/src/board/mod.rs
@@ -1,1 +1,21 @@
+use super::network::connect;
+use color_eyre::eyre::{eyre, Context, Result};
+
 use firmata::asynchronous::boardio::BoardIo;
+use tokio::net::{
+    tcp::{OwnedReadHalf, OwnedWriteHalf},
+    ToSocketAddrs,
+};
+
+pub async fn generate_board_io<A: ToSocketAddrs>(
+    addr: A,
+) -> Result<BoardIo<OwnedReadHalf, OwnedWriteHalf>> {
+    let stream = connect(addr).await?;
+    let (r, w) = stream.into_split();
+    let mut board = BoardIo::create(r, w);
+    board
+        .generate_board_state()
+        .await
+        .wrap_err_with(|| eyre!("failed to generate board state when constructing BoardIo"))?;
+    Ok(board)
+}
