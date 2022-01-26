b57c4074820c239926216f983aa54bf976cb199c
MajorArkwolf <petercrabbe92@gmail.com>
2022-01-26 14:43:32 +0800

    fix up circuits for new api

--- Operation/src/circuit/capture.rs
+++ Operation/src/circuit/capture.rs
@@ -7,7 +7,7 @@ use async_trait::async_trait;
 use circuit::{Circuit, CircuitState};
 use color_eyre::eyre::{eyre, Context, Error, Result, WrapErr};
 use color_eyre::Report;
-use sensor::{photo_resistor::PhotoResistorSubscriber, servo::ServoPublisher};
+use sensor::{photo_resistor::PhotoResistor, servo::Servo};
 use tracing::{debug, error, info};
 use tracing::{event, instrument, Level};
 
@@ -23,9 +23,9 @@ enum CaptureStates {
 pub struct Capture {
     id: u32,
     state: CircuitState,
-    servo: ServoPublisher,
-    photo_resistor: PhotoResistorSubscriber,
-    trigger: i32,
+    servo: Servo,
+    photo_resistor: PhotoResistor,
+    trigger: u16,
     internal_state: CaptureStates,
 }
 
@@ -54,25 +54,27 @@ impl Circuit for Capture {
     }
 
     #[instrument]
-    async fn update(&mut self) {
+    async fn update(&mut self) -> Result<()> {
         match self.state {
             CircuitState::Ready => self.process_ready(),
             CircuitState::Running => self.process_running().await,
             CircuitState::Waiting => self.process_waiting(),
-            CircuitState::Stopped => return,
+            CircuitState::Stopped => Err(eyre!("capture system `{}` is stopped", self.id)),
         }
     }
 
-    async fn stop(&mut self) {}
+    fn stop(&mut self) {
+        self.state = CircuitState::Stopped;
+    }
 }
 
 impl Capture {
     pub fn create(
         id: u32,
         state: CircuitState,
-        servo: ServoPublisher,
-        photo_resistor: PhotoResistorSubscriber,
-        trigger: i32,
+        servo: Servo,
+        photo_resistor: PhotoResistor,
+        trigger: u16,
     ) -> Self {
         Self {
             id,
@@ -85,14 +87,15 @@ impl Capture {
     }
 
     #[instrument(skip_all)]
-    fn process_ready(&mut self) {
+    fn process_ready(&mut self) -> Result<()> {
         self.state = CircuitState::Running;
         self.internal_state = CaptureStates::TakePicture;
         info!("capture system started");
+        Ok(())
     }
 
     #[instrument(skip_all)]
-    async fn process_running(&mut self) {
+    async fn process_running(&mut self) -> Result<()> {
         // Take Picture
         match self.internal_state {
             CaptureStates::TakePicture => {
@@ -100,67 +103,46 @@ impl Capture {
             }
             CaptureStates::RunOCR => {
                 let contents: Vec<u8> = vec![];
-                let network_ocr = Network::connect("127.0.0.1:10000").await;
-                match network_ocr {
-                    Ok(mut network) => {
-                        let card = Request::CardData(CardData {
-                            type_of: PictureFormat::TakePicture,
-                            data: contents,
-                        });
-                        network.send(card).await.unwrap();
-                        let resp = network.recv().await.unwrap();
-                        if resp.error == 0 {
-                            network.send(Request::EndConnection).await.unwrap();
-                            self.internal_state = CaptureStates::ReleaseCard;
-                        } else {
-                            error!("OCR network returned {}, stopping circuit", resp.error);
-                            self.state = CircuitState::Stopped;
-                            return;
-                        }
-                    }
-                    Err(_) => {
-                        self.state = CircuitState::Stopped;
-                        self.internal_state = CaptureStates::Finished;
-                        return;
-                    }
+                let mut network_ocr = Network::connect("127.0.0.1:10000").await?;
+                let card = Request::CardData(CardData {
+                    type_of: PictureFormat::TakePicture,
+                    data: contents,
+                });
+                network_ocr.send(card).await?;
+                let resp = network_ocr.recv().await?;
+                if resp.error == 0 {
+                    network_ocr.send(Request::EndConnection).await?;
+                    self.internal_state = CaptureStates::ReleaseCard;
+                } else {
+                    let error_msg =
+                        format!("OCR network returned {}, stopping circuit", resp.error);
+                    error!("{}", error_msg);
+                    self.state = CircuitState::Stopped;
+                    return Err(eyre!(error_msg));
                 }
                 self.internal_state = CaptureStates::ReleaseCard;
             }
             CaptureStates::ReleaseCard => {
-                info!("capture system dispending card");
+                info!("capture system releasing card");
                 let result = self.servo.set(90).await;
-                self.handle_result(result);
                 tokio::time::sleep(std::time::Duration::from_millis(1500)).await;
                 self.internal_state = CaptureStates::Finished;
             }
             CaptureStates::Finished => {
-                let value = self.photo_resistor.get().await;
-                let value = match value {
-                    Ok(v) => v,
-                    Err(_) => return,
-                };
+                let value = self.photo_resistor.get()?;
                 debug!("Photoresistor: {}", value);
                 if value > self.trigger {
                     let result = self.servo.set(0).await;
-                    self.handle_result(result);
                     tokio::time::sleep(std::time::Duration::from_millis(1500)).await;
                     self.state = CircuitState::Waiting;
                     info!("capture system moving to waiting");
                 }
             }
         }
+        Ok(())
     }
 
-    fn process_waiting(&mut self) {}
-
-    #[instrument(skip_all)]
-    fn handle_result(&mut self, result: Result<(), Report>) {
-        match result {
-            Ok(_) => {}
-            Err(_) => {
-                error!("error discovered, stopping self {:?}", self);
-                self.state = CircuitState::Stopped
-            }
-        }
+    fn process_waiting(&mut self) -> Result<()> {
+        Ok(())
     }
 }
