6dafaf911f1d638c98fd1a2f2e6ef67a5069de07
MajorArkwolf <petercrabbe92@gmail.com>
2022-01-26 22:49:48 +0800

    fix up constructor

--- Operation/src/circuit/circuit_constructor.rs
+++ Operation/src/circuit/circuit_constructor.rs
@@ -4,79 +4,50 @@ use std::vec;
 use super::circuit_controller::CircuitController;
 use super::Circuit;
 use super::CircuitState;
-use crate::board;
 use crate::circuit;
+use crate::factory::System;
 use crate::sensor::led_strip::PixelColor;
-use board::BoardContainer;
 use circuit::{capture::Capture, feeder::Feeder};
 use color_eyre::eyre::eyre;
 use color_eyre::eyre::Result;
-use firmata::Firmata;
 use tokio::sync::Mutex;
 use tracing::debug;
 
 pub struct CalibrationResult {
     sensor_id: u32,
-    sensor_calibration_value: i32,
+    sensor_calibration_value: u16,
 }
 
 async fn calibrate_photo_resistor(
-    boards: &mut BoardContainer,
+    system: &mut System,
     sensor_id: u32,
-    light_sensor_id: u32,
+    _light_sensor_id: u32,
 ) -> Result<CalibrationResult> {
     debug!("Beginning calibration of photo resistor");
-    let photo_pin = boards
-        .get_sensor(sensor_id)?
-        .as_photo_resistor_mut()
-        .unwrap()
-        .get_pin_id();
+    let photo_pin = &mut system.sensors.photo_resistor[0];
+    let led_strip = &mut system.sensors.led_strips[0];
 
-    let board1 = {
-        let board = boards.get_board_from_sensor_id(sensor_id)?;
-
-        match board.get_board() {
-            board::BoardTypes::SerialBoard(v) => v.board.clone(),
-        }
-    };
-
-    let board2 = {
-        let board = boards.get_board_from_sensor_id(light_sensor_id)?;
-
-        match board.get_board() {
-            board::BoardTypes::SerialBoard(v) => v.board.clone(),
-        }
-    };
-
-    let mut board1 = board1.lock().await;
-    let mut board2 = board2.lock().await;
+    let on = PixelColor::new(-1, 255, 255, 255);
+    let off = PixelColor::new(-1, 0, 0, 0);
 
     debug!("Turning light off");
-    let off = PixelColor::new(-1, 0, 0, 0);
     //turn light off
-    board2.string_write(&off.to_string())?;
+    led_strip.set(off).await?;
     debug!("Off msg: {}", off.to_string());
 
     let mut off_average = 0;
-    board1.poll(5)?;
     for _i in 1..20 {
-        board1.poll(5)?;
-        let pin = board1.get_physical_pin(photo_pin)?;
-        off_average += pin.value;
+        off_average += photo_pin.get()?;
+        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
     }
     off_average /= 20;
 
-    let on = PixelColor::new(-1, 255, 255, 255);
     debug!("Turning light on");
     //turn light on
-    board2.string_write(&on.to_string())?;
-
-    board1.poll(5)?;
+    led_strip.set(on).await?;
     let mut on_average = 0;
     for _i in 1..20 {
-        board1.poll(5)?;
-        let pin = board1.get_physical_pin(photo_pin)?;
-        on_average += pin.value;
+        on_average += photo_pin.get()?;
     }
     on_average /= 20;
 
@@ -90,7 +61,7 @@ async fn calibrate_photo_resistor(
         ));
     }
 
-    let half_way_point: i32 = ((on_average - off_average) / 2) + off_average;
+    let half_way_point: u16 = ((on_average - off_average) / 2) + off_average;
     debug!("Half way point decided at: {}", half_way_point);
 
     if half_way_point > on_average {
@@ -107,28 +78,35 @@ async fn calibrate_photo_resistor(
     })
 }
 
-pub async fn calibrate_sensors(boards: &mut BoardContainer) -> Result<Vec<CalibrationResult>> {
+pub async fn calibrate_sensors(system: &mut System) -> Result<Vec<CalibrationResult>> {
     debug!("Beginning sensor calibration");
     let calibration_results: Vec<CalibrationResult> =
-        vec![calibrate_photo_resistor(boards, 2, 4).await?];
+        vec![calibrate_photo_resistor(system, 2, 4).await?];
     Ok(calibration_results)
 }
 
 fn construct_feeder(
-    boards: &mut BoardContainer,
+    system: &mut System,
     calibration_results: &[CalibrationResult],
 ) -> Result<Feeder> {
-    let motor_cont = boards
-        .get_sensor(3)?
-        .as_motor_controller_mut()
-        .unwrap()
-        .publisher();
-    let photo = boards
-        .get_sensor(2)?
-        .as_photo_resistor_mut()
-        .unwrap()
-        .subscribe();
-    let servo = boards.get_sensor(1)?.as_servo_mut().unwrap().publisher();
+    let motor_cont = system
+        .sensors
+        .motor_controllers
+        .iter()
+        .find_map(|v| if *v.id() == 3 { Some(v) } else { None })
+        .ok_or_else(|| eyre!("did not find motor controller with the given id"))?;
+    let servo = system
+        .sensors
+        .servos
+        .iter()
+        .find_map(|v| if *v.id() == 1 { Some(v) } else { None })
+        .ok_or_else(|| eyre!("did not find motor controller with the given id"))?;
+    let photo = system
+        .sensors
+        .photo_resistor
+        .iter()
+        .find_map(|v| if *v.id() == 2 { Some(v) } else { None })
+        .ok_or_else(|| eyre!("did not find photo resistor with the given id"))?;
 
     let value = calibration_results
         .iter()
@@ -141,23 +119,29 @@ fn construct_feeder(
     Ok(Feeder::create(
         0,
         CircuitState::Waiting,
-        motor_cont,
-        photo,
-        servo,
+        motor_cont.clone(),
+        photo.clone(),
+        servo.clone(),
         value,
     ))
 }
 
 fn construct_capture(
-    boards: &mut BoardContainer,
+    system: &mut System,
     calibration_results: &[CalibrationResult],
 ) -> Result<Capture> {
-    let servo = boards.get_sensor(1)?.as_servo_mut().unwrap().publisher();
-    let photo = boards
-        .get_sensor(2)?
-        .as_photo_resistor_mut()
-        .unwrap()
-        .subscribe();
+    let servo = system
+        .sensors
+        .servos
+        .iter()
+        .find_map(|v| if *v.id() == 1 { Some(v) } else { None })
+        .ok_or_else(|| eyre!("did not find motor controller with the given id"))?;
+    let photo = system
+        .sensors
+        .photo_resistor
+        .iter()
+        .find_map(|v| if *v.id() == 2 { Some(v) } else { None })
+        .ok_or_else(|| eyre!("did not find photo resistor with the given id"))?;
 
     let value = calibration_results
         .iter()
@@ -168,23 +152,23 @@ fn construct_capture(
     Ok(Capture::create(
         1,
         CircuitState::Waiting,
-        servo,
-        photo,
+        servo.clone(),
+        photo.clone(),
         value,
     ))
 }
 
 pub fn construct_circuit(
-    boards: &mut BoardContainer,
+    system: &mut System,
     calibration_results: &[CalibrationResult],
 ) -> Result<CircuitController> {
     let circuits: Vec<Arc<Mutex<Box<dyn Circuit + Send>>>> = vec![
         Arc::new(Mutex::new(Box::new(construct_feeder(
-            boards,
+            system,
             calibration_results,
         )?))),
         Arc::new(Mutex::new(Box::new(construct_capture(
-            boards,
+            system,
             calibration_results,
         )?))),
     ];
