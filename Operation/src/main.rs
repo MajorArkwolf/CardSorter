d55e984eeaa36f1104d54a694c41b7832f2b2465
Arkwolf <petercrabbe92@gmail.com>
2022-04-05 15:30:11 +0800

    update main

--- Operation/src/main.rs
+++ Operation/src/main.rs
@@ -6,8 +6,11 @@ pub mod factory;
 pub mod io;
 pub mod network;
 pub mod sensor;
-use color_eyre::eyre::Result;
+use futures::stream::FuturesUnordered;
+use tokio::task::JoinHandle;
 use tracing::{debug, info};
+use tokio_stream::StreamExt;
+use color_eyre::eyre::{eyre, Context, Result};
 
 use crate::backbone::overseer::Overseer;
 //use std::time::{Duration, Instant};
@@ -18,16 +21,55 @@ async fn main() -> Result<()> {
     //console_subscriber::init();
     info!("System startup initiated");
     let mut overseer = Overseer::create();
+    let overseer_channel = overseer.get_comm_channels();
 
     let mut system = factory::generate_system().await?;
     let mut circuit_watcher =
-        circuit::factory::generate_circuits(&mut overseer, &mut system).await?;
+        circuit::factory::generate_circuits(overseer_channel, &mut system).await?;
     //let calibration_results = circuit_constructor::calibrate_sensors(&mut system).await?;
     //let mut circuit_controller =
     //    circuit_constructor::construct_circuit(&mut system, &calibration_results).await?;
 
-    //circuit_controller.start().await?;
-    debug!("Beginning circuit controller update cycle");
-    circuit_watcher.run().await?;
+    let mut main_tasks: FuturesUnordered<JoinHandle<Result<()>>> = FuturesUnordered::new();
+    info!("Beginning circuit watcher task.");
+    main_tasks.push(
+        tokio::task::spawn(async move {
+        circuit_watcher.run().await?;
+        debug!("circuit tasked ended");
+        Ok(())
+    }));
+
+    info!("Beginning overseer task.");
+    main_tasks.push(
+        tokio::task::spawn(async move {
+        loop {
+            overseer.run().await?;
+        }
+        debug!("overseer tasked ended");
+        Ok(())
+    }));
+    info!("Task creation complete, system running");
+
+    loop {
+        while let Some(task) = main_tasks.next().await {
+            match task {
+                Ok(v) => {
+                    match v {
+                        Ok(_) => {
+                            debug!("a task ended with an ok result");
+                        },
+                        Err(err) => {
+                            return Err(eyre!("task returned a failrue: {:?}", err));
+                        },
+                    }
+                    
+                },
+                Err(err) => {
+                    return Err(eyre!("task failed to join, terminating program: {:?}", err));
+                },
+            }
+        }
+    }
+    
     Ok(())
 }
