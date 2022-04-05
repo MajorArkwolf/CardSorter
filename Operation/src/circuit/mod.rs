7a959706200107815ff3d254e10774c2f9077a5d
Arkwolf <petercrabbe92@gmail.com>
2022-04-05 13:54:46 +0800

    expand circuit watcher

--- Operation/src/circuit/mod.rs
+++ Operation/src/circuit/mod.rs
@@ -1,10 +1,12 @@
 pub mod capture;
 pub mod factory;
 pub mod feeder;
+use futures::{stream::FuturesUnordered};
 use async_trait::async_trait;
-use color_eyre::eyre::{eyre, Report, Result};
+use color_eyre::eyre::{eyre, Result};
 use tokio::{sync::watch, task::JoinHandle};
-use tracing::{debug, info};
+use tokio_stream::StreamExt;
+use tracing::{info, debug, error};
 
 #[async_trait]
 pub trait Circuit {
@@ -30,32 +32,101 @@ pub enum Type {
     Dispensor,
 }
 
-pub struct Template {
-    id: u32,
-    type_of: Type,
-}
-
 pub struct CircuitWatcher {
+    current_state: State,
     watch_tx: watch::Sender<State>,
-    join_handles: Vec<JoinHandle<Result<()>>>,
+    sub_tasks: FuturesUnordered<JoinHandle<Result<()>>>,
 }
 
 impl CircuitWatcher {
+    pub fn add_join_handle(&mut self, joinhandle: JoinHandle<Result<()>>) {
+        self.sub_tasks.push(joinhandle);
+    }
+
     pub fn create(watch_tx: watch::Sender<State>) -> Self {
         Self {
+            current_state: State::Waiting,
             watch_tx,
-            join_handles: vec![],
+            sub_tasks: FuturesUnordered::new(),
         }
     }
 
-    pub fn add_join_handle(&mut self, joinhandle: JoinHandle<Result<()>>) {
-        self.join_handles.push(joinhandle);
+    async fn process_tasks(&mut self) -> State {
+        while let Some(item) = self.sub_tasks.next().await {
+            match item {
+                Ok(res) => { 
+                    match res {
+                        Ok(()) => {
+                            debug!("Task returned ending.");
+                            return State::Ending;
+                        },
+                        Err(err) => {
+                            error!("Task returned error: {}", err);
+                            return State::Stop;
+                        },
+                    }
+                },
+                Err(err) => {
+                    error!("Task join error: {}", err);
+                    return State::Stop;
+                },
+            }
+        }
+        debug!("Fell through, returning waiting.");
+        return State::Waiting;
+    }
+
+    fn should_stop(state: State) -> bool {
+        match state {
+            State::Waiting | State::Running => false,
+            State::Stop | State::Ending => true,
+        }
+    }
+
+    pub fn kill_all_tasks(&mut self) {
+        debug!("kill all tasks called");
+        self.sub_tasks.clear();
     }
 
     pub async fn run(&mut self) -> Result<()> {
         info!("Starting circuits.");
-        self.watch_tx.send(State::Running)?;
-        loop {}
+        self.current_state = State::Running;
+        self.watch_tx.send(self.current_state)?;
+
+        info!("Began watching tasks.");
+        while CircuitWatcher::should_stop(self.current_state) {
+            let result = self.process_tasks().await;
+            if self.current_state != State::Stop {
+                self.current_state = result;
+            }
+            self.watch_tx.send(self.current_state)?;
+        }
+
+        info!("Task loop has been broken, beginning clean up.");
+        match self.current_state {
+            State::Waiting | State::Running => {},
+            State::Stop => {
+                self.watch_tx.send(self.current_state)?;
+                self.kill_all_tasks();
+                return Err(eyre!("A stop state was recieved from a task, this should not occur."));
+            },
+            State::Ending => {
+                self.watch_tx.send(self.current_state)?;
+                loop {
+                    let result = self.process_tasks().await;
+                    match result {
+                        State::Running | State::Stop => {
+                            error!("While attempting to resolve a ending state, a {:?} state was returned. This is undefined, terminating tasks", result);
+                            self.kill_all_tasks();
+                            return Err(eyre!("A stop state was returned when trying to resolve an end state."));
+                        },
+                        State::Ending => continue, // Keep looping until a waiting is returned.
+                        State::Waiting => break, // Waiting is the fall through statement that occures when no tasks are left.
+                    }
+                }
+            },
+        }
+        debug!("Run task ran to completion.");
         Ok(())
     }
 }
